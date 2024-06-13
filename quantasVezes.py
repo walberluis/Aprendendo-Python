1a9f527869476bcfeab526e5c8492c57761155ec
Walber Luis Santos da Paixão <144743573+walberluis@users.noreply.github.com>
2024-06-13 17:04:35 -0300

    Update quantasVezes.py

--- quantasVezes.py
+++ quantasVezes.py
@@ -1,3 +1,6 @@
+# Leia um inteiro N.
+# Depois, leia até 20 inteiros X0, X1, ..., X20. Seu programa deve imprimir quantas vezes o inteiro N aparece nos X inteiros.
+
 i = int(input())
 caixinha = 0
 for n in range (0, 20):
